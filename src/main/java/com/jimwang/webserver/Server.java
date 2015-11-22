be5c11a3c7823f9029908d5b69f8edb5e21f46c7
jimbo-w <jimwang86@gmail.com>
2015-11-22 22:11:05 +0000

    Making a trivial change to test GitHub integration

--- src/main/java/com/jimwang/webserver/Server.java
+++ src/main/java/com/jimwang/webserver/Server.java
@@ -19,6 +19,8 @@ public class Server extends AbstractVerticle {
     @Override
     public void start() throws Exception {
 
+        // Making a trivial change
+
         Vertx vertx = Vertx.vertx();
         Router router = Router.router(vertx);
 
